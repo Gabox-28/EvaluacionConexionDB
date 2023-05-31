8dedb04d2e75ea7cfb8ea58401b386ebcfff5cbb
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-05-31 16:19:38 -0600

    Evaluacion terminada

--- app/src/main/java/ni/edu/uca/sistematicopersistencia/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/sistematicopersistencia/MainActivity.kt
@@ -2,6 +2,10 @@ package ni.edu.uca.sistematicopersistencia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import kotlinx.coroutines.launch
 import ni.edu.uca.sistematicopersistencia.data.database.BaseDatos
 import ni.edu.uca.sistematicopersistencia.data.database.entities.EntityProducto
 import ni.edu.uca.sistematicopersistencia.databinding.ActivityMainBinding
@@ -14,6 +18,51 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        room = Room.databaseBuilder(this, BaseDatos::class.java, "dbEvaluacion").build()
+        obtenerProducto(room)
+
+        with(binding) {
+            binding.btnAgregar.setOnClickListener {
+                if (etNombre.text.toString() == "" || etPrecio.text.toString() == "" || etExistencia.text.toString() == "") {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Todos los campos son requeridos",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+
+                    producto = EntityProducto(
+                        etNombre.text.toString().trim(),
+                        etPrecio.text.toString().toDouble(),
+                        etExistencia.text.toString().toInt(),
+                    )
+
+                    agregarProducto(room, producto)
+                    Toast.makeText(this@MainActivity, "Guardado Exitoso", Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }
+
+
+    }
+
+    private fun obtenerProducto(room: BaseDatos) {
+        lifecycleScope.launch {
+            listaProd = room.productoDao().obtenerProducto()
+            binding.tvDatosProducto.text = listaProd.toString()
+        }
+    }
+
+    private fun agregarProducto(room: BaseDatos, producto: EntityProducto) {
+        lifecycleScope.launch {
+            room.productoDao().agregarProducto(producto)
+            obtenerProducto(room)
+        }
     }
 }
