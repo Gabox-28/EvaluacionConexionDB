37a3ae4f7c269f3cdfe143b87eeb1f054e7b0134
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-05-31 16:01:44 -0600

    Vista terminada y correciones en la base de datos

--- app/src/main/java/ni/edu/uca/sistematicopersistencia/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/sistematicopersistencia/MainActivity.kt
@@ -2,8 +2,16 @@ package ni.edu.uca.sistematicopersistencia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ni.edu.uca.sistematicopersistencia.data.database.BaseDatos
+import ni.edu.uca.sistematicopersistencia.data.database.entities.EntityProducto
+import ni.edu.uca.sistematicopersistencia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var listaProd: MutableList<EntityProducto> = mutableListOf()
+    lateinit var room: BaseDatos
+    lateinit var producto: EntityProducto
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
